48a198db9f32fb215c1a79be3a435a4d722fbf54
Walter Zimmerman <zimmermanw84@gmail.com>
2016-10-21 16:55:57 -0700

    intialize models

---
+++ models/frequency_request.js
@@ -0,0 +1,13 @@
+//
+//  controllers/frequency_request.js
+//
+//  Created by Walt Zimmerman on 8/24/16.
+//
+
+"use strict";
+
+module.exports = class FrequencyRequest {
+  constructor() {
+
+  }
+};
